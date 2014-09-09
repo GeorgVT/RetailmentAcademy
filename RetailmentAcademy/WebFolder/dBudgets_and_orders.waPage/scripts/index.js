47217d26643a9bef219bd816305a55831f8741f3
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-09-09 20:08:35 +0700

    Simple login events
    + В зависимости от состояния логина окрашивается фон контейнера (логика окраски расположена в коде логина)

--- RetailmentAcademy/WebFolder/dBudgets_and_orders.waPage/scripts/index.js
+++ RetailmentAcademy/WebFolder/dBudgets_and_orders.waPage/scripts/index.js
@@ -2,10 +2,45 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var loginBar = {};	// @login
+	var documentEvent = {};	// @document
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	loginBar.logout = function loginBar_logout (event)// @startlock
+	{// @endlock
+		vUser = WAF.directory.currentUser();
+		if(vUser == null) {
+			$$('testContainer').setBackgroundColor("red");
+			this.showLoginDialog();
+		}
+
+	};// @lock
+
+	loginBar.login = function loginBar_login (event)// @startlock
+	{// @endlock
+		vUser = WAF.directory.currentUser();
+		if(vUser != null) {
+			$$('testContainer').setBackgroundColor("blue")
+		}
+
+	};// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		vUser = WAF.directory.currentUser();
+		if(vUser == null) {
+			$$('testContainer').setBackgroundColor("red")
+			$$('loginBar').showLoginDialog();
+		} else {
+			$$('testContainer').setBackgroundColor("blue")
+		}
+	};// @lock
+
 // @region eventManager// @startlock
+	WAF.addListener("loginBar", "logout", loginBar.logout, "WAF");
+	WAF.addListener("loginBar", "login", loginBar.login, "WAF");
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 // @endregion
 };// @endlock
