346a7ae70b22d1e9bde48a77f587ce80f5d664c3
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-09-11 15:41:59 +0700

    !!!CRASHEABLE events in main window!!!
    Этот механизм не обходимо ДОДЕЛАТЬ:
     - разорвать замыкания при обновлении логина
     - перечитать javascript.ru как правильно обрабатывать события

--- RetailmentAcademy/WebFolder/dBudgets_and_orders.waPage/scripts/index.js
+++ RetailmentAcademy/WebFolder/dBudgets_and_orders.waPage/scripts/index.js
@@ -9,47 +9,94 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	var documentEvent = {};	// @document
 // @endregion// @endlock
 
+
+// @region customWidgetFunctions
+//	WAF.widget.Container.prototype.onLoggedStatusChanged = function(isLoggedIn) {
+//		if(this.id == 'testContainer') {
+//			if (! isLoggedIn) this.destroy();
+////			else this.rebuild();
+//		}
+//	};
+
+//	WAF.widget.login.prototype.updateCurrentUser = function() {
+////		alert("Hi " + cUser + "!");
+//		cUser = WAF.directory.currentUser();
+//		
+//	}
+// @endregion
+
 	cUser = WAF.directory.currentUser();
+
+// @region customEventHandlers
+	$(document).off(".doc");
+	
+	$(document).bind("evLoginStatusChanged.doc", function (evt) {
+
+		if(! (evt.isLoggedIn)) {
+			//Let's destroy main widgets on our page
+			$$('testContainer').destroy();
+			$$('mainMenuBar').destroy();
+			$$('loginBar').showLoginDialog();
+		}
+	});
+// @endregion
+
 
 // eventHandlers// @lock
 
 	loginBar.logout = function loginBar_logout (event)// @startlock
 	{// @endlock
 		cUser = WAF.directory.currentUser();
-		$$('testContainer').onLoggedStatusChanged(cUser != null);
-		if(cUser == null) this.showLoginDialog();
+//		this.updateCurrentUser();
+
+//		$$('testContainer').onLoggedStatusChanged(cUser != null);
+//		if(cUser == null) this.showLoginDialog();
+		$(document).trigger({
+			type:"evLoginStatusChanged.doc",
+			isLoggedIn: (cUser != null)
+		});
 
 	};// @lock
 
 	loginBar.login = function loginBar_login (event)// @startlock
 	{// @endlock
 		cUser = WAF.directory.currentUser();
-		
-		//Let's reload current page
-		if (cUser != null) {
+//		this.updateCurrentUser();
+
+		//Let's process logged in status change
+		//$$('testContainer').onLoggedStatusChanged(cUser != null);
+		$(document).trigger({
+			type:"evLoginStatusChanged.doc",
+			isLoggedIn: (cUser != null)
+		});
+
+		if(cUser != null) {
+			//Let's reload current page
 			var strTemp = window.location;
 			window.location = strTemp;
 		}
 
-		//Let's process logged in status change
-		$$('testContainer').onLoggedStatusChanged(cUser != null);
 	};// @lock
+
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
 		cUser = WAF.directory.currentUser();
-		$$('testContainer').onLoggedStatusChanged(cUser != null);
-		if(cUser == null) $$('loginBar').showLoginDialog();
-	};// @lock
+		
+//		alert("Loaded!");
+		
+//		$$('loginBar').updateCurrentUser();
+				
+//		$$('testContainer').onLoggedStatusChanged(cUser != null);
+		$(document).trigger({
+			type:"evLoginStatusChanged.doc",
+			isLoggedIn: (cUser != null)
+		});
 
-// @region customWidgetFunctions
-	WAF.widget.Container.prototype.onLoggedStatusChanged = function(isLoggedIn) {
-		if(this.id == 'testContainer') {
-			if (! isLoggedIn) this.destroy();
-//			else this.rebuild();
+		if(cUser == null) {
+			$$('loginBar').showLoginDialog();
 		}
-	};
-// @endregion
+	};// @lock
 
 // @region eventManager// @startlock
 	WAF.addListener("loginBar", "logout", loginBar.logout, "WAF");
