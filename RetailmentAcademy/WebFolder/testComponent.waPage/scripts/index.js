1e162a92836f1ef186100899814a349b55644b05
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-09-12 20:34:24 +0700

    Initial component test

---
+++ RetailmentAcademy/WebFolder/testComponent.waPage/scripts/index.js
@@ -0,0 +1,23 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		$$('mainComponent').loadComponent({
+			userData: { sezonZakazov: 'Лето 2015' },
+			osSuccess: function () {
+							alert("Загрузка успешно завершена!");
+						}
+		});
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
