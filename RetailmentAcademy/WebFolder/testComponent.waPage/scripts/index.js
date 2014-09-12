527d0a754f78c3c8ed1eea7156a3e83e86ad86e6
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-09-12 20:48:43 +0700

    Basic web-component invocation
    + подправлена верстка компоненты, чтобы не съезжали размеры элементов
    + добавлена принудительная загрузка веб-компоненты и контроль ее корректногг завершения

--- RetailmentAcademy/WebFolder/testComponent.waPage/scripts/index.js
+++ RetailmentAcademy/WebFolder/testComponent.waPage/scripts/index.js
@@ -9,9 +9,10 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
+		alert('Начали!');
 		$$('mainComponent').loadComponent({
 			userData: { sezonZakazov: 'Лето 2015' },
-			osSuccess: function () {
+			onSuccess: function onComponentSuccessfullyLoaded () {
 							alert("Загрузка успешно завершена!");
 						}
 		});
