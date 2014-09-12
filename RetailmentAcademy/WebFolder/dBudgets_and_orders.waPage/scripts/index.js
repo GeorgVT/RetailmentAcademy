7b4c73410de43a725ae6713519462e3e08a1010d
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-09-12 19:54:17 +0700

    MainMenu item events dispatcher completed
    На все пункты меню навешаны осмысленные идентификаторы и обработчики onclick
    Реальная обработка событий (управление закладками) пока НЕ СДЕЛАНА!

--- RetailmentAcademy/WebFolder/dBudgets_and_orders.waPage/scripts/index.js
+++ RetailmentAcademy/WebFolder/dBudgets_and_orders.waPage/scripts/index.js
@@ -5,56 +5,165 @@ var cUserGroups = {};
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var menuRoliVSysteme = {};	// @menuItem
+	var menuItemValuty = {};	// @menuItem
+	var menuItemStaticheskiyKlassifikator = {};	// @menuItem
+	var menuItemRaspredelenieBudgeta = {};	// @menuItem
+	var menuItemPlanFact = {};	// @menuItem
+	var menuOtchety = {};	// @menuItem
+	var menuItemZakazy = {};	// @menuItem
+	var menuBudgetyIZakazy = {};	// @menuItem
+	var menuItemBudgety = {};	// @menuItem
+	var mainMenuBar = {};	// @menuBar
 	var loginBar = {};	// @login
 	var documentEvent = {};	// @document
 // @endregion// @endlock
 
-	cUser = WAF.directory.currentUser();
-
 // eventHandlers// @lock
+
+	menuRoliVSysteme.click = function menuRoliVSysteme_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuItemValuty.click = function menuItemValuty_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuItemStaticheskiyKlassifikator.click = function menuItemStaticheskiyKlassifikator_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuItemRaspredelenieBudgeta.click = function menuItemRaspredelenieBudgeta_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuItemPlanFact.click = function menuItemPlanFact_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuOtchety.click = function menuOtchety_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuItemZakazy.click = function menuItemZakazy_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuBudgetyIZakazy.click = function menuBudgetyIZakazy_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuItemBudgety.click = function menuItemBudgety_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	mainMenuBar.click = function mainMenuBar_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
 
 	loginBar.logout = function loginBar_logout (event)// @startlock
 	{// @endlock
-		cUser = WAF.directory.currentUser();
-//		$$('testContainer').onLoggedStatusChanged(cUser != null);
-		this.onLoggedStatusChanged(cUser != null);
-		if(cUser == null) this.showLoginDialog();
-
+		$$('loginBar').onLoggedStatusChanged();
+		
+		return false;
 	};// @lock
 
 	loginBar.login = function loginBar_login (event)// @startlock
 	{// @endlock
-		cUser = WAF.directory.currentUser();
-		
 		//Let's reload current page
-		if (cUser != null) {
+		if (WAF.directory.currentUser() != null) {
 			var strTemp = window.location;
 			window.location = strTemp;
 		}
-
-		//Let's process logged in status change
-		this.onLoggedStatusChanged(cUser != null);
-
 	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
-		cUser = WAF.directory.currentUser();
-//		$$('testContainer').onLoggedStatusChanged(cUser != null);
-		$$('loginBar').onLoggedStatusChanged(cUser != null);
-		if(cUser == null) $$('loginBar').showLoginDialog();
+		$$('loginBar').onLoggedStatusChanged();
 	};// @lock
 
-// @region customWidgetFunctions
-	WAF.widget.Login.prototype.onLoggedStatusChanged = function(isLoggedIn) {
-		if (! isLoggedIn) {
-			$$('testContainer').destroy();
-			$$('mainMenuBar').destroy();
+// @region customLoginWidgetFunctions
+
+	WAF.widget.Login.prototype.onLoggedStatusChanged = function() {
+		if(this.id == 'loginBar') {
+			cUser = WAF.directory.currentUser();
+			
+			if (cUser != null) {
+				//??Update user rights list based on group membership
+
+				//Let's process logged in status change
+				$$('mainMenuBar').onLoggedStatusChanged(cUser != null);
+			} else {
+				$$('mainMenuBar').onLoggedStatusChanged(cUser != null);
+				$$('mainMenuBar').destroy();
+				
+				this.showLoginDialog();
+			}
 		}
 	};
+
+// @endregion
+
+// @region customMenuBarWidgetFunctions
+
+	WAF.widget.MenuBar.prototype.onLoggedStatusChanged = function(isLoggedIn) {
+		if(this.id == 'mainMenuBar') {
+			if (! isLoggedIn) {
+				$$('mainContainer').onLoggedStatusChanged(cUser != null);
+				$$('mainContainer').destroy();
+			} else {
+				//Initialize main menu items visibility based on user's groups
+			}
+		}
+	};
+
+	WAF.widget.MenuBar.prototype.processEvents = function(event) {
+		
+		if(this.id == 'mainMenuBar') {
+			alert("Event.type: " + event.type 
+								+ "\nTarget: "+event.currentTarget.id 
+								+ " (of kind: " + $$(event.currentTarget.id).kind + ")");
+			//if(event.Type == "click") alert("Target: "+event.target.id);
+			
+			return false;
+		}
+	};
+
+// @endregion
+
+// @region customContainerWidgetFunctions
+
+	WAF.widget.Container.prototype.onLoggedStatusChanged = function(isLoggedIn) {
+		if(this.id == 'mainContainer') {
+			if (isLoggedIn) {
+				//Initialize tabs visibility based on main menu items availability
+			}
+		}
+	};
+
 // @endregion
 
 // @region eventManager// @startlock
+	WAF.addListener("menuRoliVSysteme", "click", menuRoliVSysteme.click, "WAF");
+	WAF.addListener("menuItemValuty", "click", menuItemValuty.click, "WAF");
+	WAF.addListener("menuItemStaticheskiyKlassifikator", "click", menuItemStaticheskiyKlassifikator.click, "WAF");
+	WAF.addListener("menuItemRaspredelenieBudgeta", "click", menuItemRaspredelenieBudgeta.click, "WAF");
+	WAF.addListener("menuItemPlanFact", "click", menuItemPlanFact.click, "WAF");
+	WAF.addListener("menuOtchety", "click", menuOtchety.click, "WAF");
+	WAF.addListener("menuItemZakazy", "click", menuItemZakazy.click, "WAF");
+	WAF.addListener("menuBudgetyIZakazy", "click", menuBudgetyIZakazy.click, "WAF");
+	WAF.addListener("menuItemBudgety", "click", menuItemBudgety.click, "WAF");
+	WAF.addListener("mainMenuBar", "click", mainMenuBar.click, "WAF");
 	WAF.addListener("loginBar", "logout", loginBar.logout, "WAF");
 	WAF.addListener("loginBar", "login", loginBar.login, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
