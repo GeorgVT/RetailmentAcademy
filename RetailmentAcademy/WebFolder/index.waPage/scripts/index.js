1f1cd5660e7ce08e0b177ab9a72d7e959914def1
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-09-19 18:08:12 +0700

    Main menu added

--- RetailmentAcademy/WebFolder/index.waPage/scripts/index.js
+++ RetailmentAcademy/WebFolder/index.waPage/scripts/index.js
@@ -4,11 +4,77 @@ var cUserGroups = {};
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var menuItemValuty = {};	// @menuItem
+	var menuItemStaticheskiyKlassifikator = {};	// @menuItem
+	var menuItemRaspredelenieBudgeta = {};	// @menuItem
+	var menuItemPlanFact = {};	// @menuItem
+	var menuItemZakazy = {};	// @menuItem
+	var menuItemBudgety = {};	// @menuItem
+	var menuRoliVSysteme = {};	// @menuItem
+	var menuKlassifikatory = {};	// @menuItem
+	var menuOtchety = {};	// @menuItem
+	var menuBudgetyIZakazy = {};	// @menuItem
+	var mainMenuBar = {};	// @menuBar
 	var documentEvent = {};	// @document
 	var loginBar = {};	// @login
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	menuItemValuty.click = function menuItemValuty_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuItemStaticheskiyKlassifikator.click = function menuItemStaticheskiyKlassifikator_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuItemRaspredelenieBudgeta.click = function menuItemRaspredelenieBudgeta_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuItemPlanFact.click = function menuItemPlanFact_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuItemZakazy.click = function menuItemZakazy_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuItemBudgety.click = function menuItemBudgety_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuRoliVSysteme.click = function menuRoliVSysteme_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuKlassifikatory.click = function menuKlassifikatory_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuOtchety.click = function menuOtchety_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	menuBudgetyIZakazy.click = function menuBudgetyIZakazy_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
+
+	mainMenuBar.click = function mainMenuBar_click (event)// @startlock
+	{// @endlock
+		return $$('mainMenuBar').processEvents(event);
+	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
@@ -41,10 +107,10 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 				//??Update user rights list based on group membership
 
 				//Let's process logged in status change
-//				$$('mainMenuBar').onLoggedStatusChanged(cUser != null);
-//			} else {
-//				$$('mainMenuBar').onLoggedStatusChanged(cUser != null);
-//				$$('mainMenuBar').destroy();
+				$$('mainMenuBar').onLoggedStatusChanged(cUser != null);
+			} else {
+				$$('mainMenuBar').onLoggedStatusChanged(cUser != null);
+				$$('mainMenuBar').destroy();
 				
 				this.showLoginDialog();
 			}
@@ -53,8 +119,48 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @endregion
 
+// @region customMenuBarWidgetFunctions
+
+	WAF.widget.MenuBar.prototype.onLoggedStatusChanged = function(isLoggedIn) {
+		if(this.id == 'mainMenuBar') {
+			if (! isLoggedIn) {
+//				$$('mainContainer').onLoggedStatusChanged(cUser != null);
+//				$$('mainContainer').destroy();
+			} else {
+				//??Initialize main menu items visibility based on user's groups
+			}
+		}
+	};
+
+	WAF.widget.MenuBar.prototype.processEvents = function(event) {
+		
+		if(this.id == 'mainMenuBar') {
+			
+			//!!
+			alert("Event.type: " + event.type 
+								+ "\nTarget: "+event.currentTarget.id 
+								+ " (of kind: " + $$(event.currentTarget.id).kind + ")");
+			//if(event.Type == "click") alert("Target: "+event.target.id);
+			
+			return false;
+		}
+	};
+
+// @endregion
+
 
 // @region eventManager// @startlock
+	WAF.addListener("menuItemValuty", "click", menuItemValuty.click, "WAF");
+	WAF.addListener("menuItemStaticheskiyKlassifikator", "click", menuItemStaticheskiyKlassifikator.click, "WAF");
+	WAF.addListener("menuItemRaspredelenieBudgeta", "click", menuItemRaspredelenieBudgeta.click, "WAF");
+	WAF.addListener("menuItemPlanFact", "click", menuItemPlanFact.click, "WAF");
+	WAF.addListener("menuItemZakazy", "click", menuItemZakazy.click, "WAF");
+	WAF.addListener("menuItemBudgety", "click", menuItemBudgety.click, "WAF");
+	WAF.addListener("menuRoliVSysteme", "click", menuRoliVSysteme.click, "WAF");
+	WAF.addListener("menuKlassifikatory", "click", menuKlassifikatory.click, "WAF");
+	WAF.addListener("menuOtchety", "click", menuOtchety.click, "WAF");
+	WAF.addListener("menuBudgetyIZakazy", "click", menuBudgetyIZakazy.click, "WAF");
+	WAF.addListener("mainMenuBar", "click", mainMenuBar.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 	WAF.addListener("loginBar", "logout", loginBar.logout, "WAF");
 	WAF.addListener("loginBar", "login", loginBar.login, "WAF");
