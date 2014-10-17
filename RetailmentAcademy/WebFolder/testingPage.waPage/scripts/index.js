1d641c6e5f112741e368480a8d5fa3c9bc8f6965
GeorgVT <georgvt@ya.ru>
2014-10-17 18:27:50 +0700

    Update #65
    Update #65

---
+++ RetailmentAcademy/WebFolder/testingPage.waPage/scripts/index.js
@@ -0,0 +1,20 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var mainAccordion = {};	// @accordion
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	mainAccordion.click = function mainAccordion_click (event)// @startlock
+	{// @endlock
+		$$('dataGrid3').redraw();
+		console.log("Click! our tag is:", $$('dataGrid3'));
+		console.info(" and has ", $$('container3'));
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("mainAccordion", "click", mainAccordion.click, "WAF");
+// @endregion
+};// @endlock
