23ee9796dfb68b2be564258b9cd1555f714b20a1
GeorgVT <georgvt@ya.ru>
2014-10-20 00:20:33 +0700

    + ShortName attribute was added for dataClass Magazin + gridMagaziny was added + gridTradeMarks

--- RetailmentAcademy/WebFolder/KlassifikatorDinamicheskiy.waComponent/KlassifikatorDinamicheskiy.js
+++ RetailmentAcademy/WebFolder/KlassifikatorDinamicheskiy.waComponent/KlassifikatorDinamicheskiy.js
@@ -13,11 +13,25 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var container5 = {};	// @container
+	var container2 = {};	// @container
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	container5.click = function container5_click (event)// @startlock
+	{// @endlock
+		$$(getHtmlId('gridMagaziny')).redraw();
+	};// @lock
+
+	container2.click = function container2_click (event)// @startlock
+	{// @endlock
+		$$(getHtmlId('gridTradeMarks')).redraw();
+	};// @lock
+
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_container5", "click", container5.click, "WAF");
+	WAF.addListener(this.id + "_container2", "click", container2.click, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
